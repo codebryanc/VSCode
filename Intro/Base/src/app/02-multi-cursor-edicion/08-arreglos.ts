d835279df1cd43a5a5ab4361153ec89c052f05a0
Bryan Cubillos <Bryant_alejandro@hotmail.com>
2020-08-24 08:51:38 -0500

    Fin de la sección 03

--- Intro/Base/src/app/02-multi-cursor-edicion/08-arreglos.ts
+++ Intro/Base/src/app/02-multi-cursor-edicion/08-arreglos.ts
@@ -11,12 +11,14 @@
 
 // Crear un arreglo con los días de la semana
 
-
-
-
+const misDiasDemo = [ 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo' ];
 
 
 // Objetivo final
 
 const diasDemo = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'];
 
+// video
+// Al seleccionar el contenido VSCode con un caracter puede hacerle Wrap
+
+const dias = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
