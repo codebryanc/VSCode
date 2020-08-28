1df6ac4481929581f3c0f2994166efce0134c4ba
Bryan Cubillos <Bryant_alejandro@hotmail.com>
2020-08-28 09:02:36 -0500

    fin sección 06

---
+++ Intro/Base/src/app/05-extensiones/07 .colorizer.ts
@@ -0,0 +1,28 @@
+import { saludar } from '../01-edicion-basica/extra/funciones';
+
+
+
+
+function() {
+
+    console.log('algo raro');
+    
+    let obj = {
+
+        nombre = "Bryan",
+        saludar: () => {
+
+
+            const obj2 = {
+
+
+            }
+        }
+
+    }
+    var color = {
+
+
+    }
+
+}
